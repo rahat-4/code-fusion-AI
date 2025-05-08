6e09c400d348c86608fe5ddad00f7a34e752789b
rahat-4 <mainul.rahat4@gmail.com>
2025-05-08 19:22:38 +0600

    Add api to retrieve details of specific country

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import CountryListCreateView
+from .views import CountryListCreateView, CountryDetailView
 
 urlpatterns = [
     path("/countries", CountryListCreateView.as_view(), name="country-list-create"),
+    path(
+        "/countries/<uuid:country_uid>",
+        CountryDetailView.as_view(),
+        name="country-detail",
+    ),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 
 from apps.countries.models import Country
 
@@ -8,3 +8,11 @@ from .serializers import CountrySerializer
 class CountryListCreateView(ListCreateAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+
+
+class CountryDetailView(RetrieveAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+    def get_object(self):
+        return self.get_queryset().get(uid=self.kwargs["country_uid"])
